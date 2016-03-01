62d87fa6b413a884333b13680fa52acbbc757c72
stanley <stanley@localhost.localdomain>
2016-03-01 16:48:35 +0800

    Add FreeSWITCH user status query

--- MyEslEventTest.java
+++ MyEslEventTest.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
- 
+
+import org.freeswitch.esl.client.transport.message.*;
 import org.freeswitch.esl.client.inbound.Client;
 import org.freeswitch.esl.client.inbound.InboundConnectionFailure;
 import org.slf4j.Logger;
@@ -17,13 +18,23 @@ public class MyEslEventTest {
     log.info("--- connect client ---");
     Client client = new Client();
     client.addEventListener(new MyEslEventListener());
-    client.connect("10.36.151.133", 8021, "ClueCon", 1);    
+    try{
+	client.connect("10.36.151.133", 8021, "ClueCon", 1);    
+    }catch(Exception e){
+	log.error( "Connect failed", e );
+	System.out.println("Error:"+e.getMessage());  
+        return;  
+    }
     //client.connect(new InetSocketAddress("10.36.151.133", 8021), "ClueCon", 1);
      
     log.info("--- event subscription ---");
     Thread.sleep(3000);
     client.setEventSubscriptions("plain", "all");
-     
+    
+    log.info("--- check registration status ---");
+    EslMessage response = client.sendSyncApiCommand("sofia xmlstatus", "profile internal reg 1002");
+    log.info( "Response to 'sofia_contact': [{}]", response );
+
     log.info("--- call 1001 1002 ---");
     client.sendSyncApiCommand("originate", "user/1001 1002");
     new Scanner(System.in).nextLine();
